52ab6730ee6643230fcff36304237c45218ea7e5
Tobias Aagaard <Tobias909@live.dk>
2023-11-30 14:24:02 +0100

    Lidt ekstra

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,12 +5,12 @@ export const About = () => {
     return (
         <section id="about">
             <article>
-            
+            <h2 className='about'>Om os</h2>
             <div className='underline'></div>
                 <figure>
+                
                     <figcaption>
-                    <h2>Om os</h2>
-                    <p>Vi er en lokal social miljøorganisation, der fokuserer på bæredygtighed og social ansvarlighed. Vi er også en socialøkonomisk non-profit virksomhed.  </p>
+                    <h2>Vi er en lokal social miljøorganisation, der fokuserer på bæredygtighed og social ansvarlighed. Vi er også en socialøkonomisk non-profit virksomhed.</h2>
                     <p>Alle ugens dage kører Nordjysk Fødevareoverskud vareture, hvor vi henter overskudsmad hos producenter, grossister og detailhandlende. Vi indsamler gode spiselige fødevarer.</p>
                     <p>De fordeles til økonomisk trængte familier, unge på lave sociale ydelser, pensionister og andre, hvor en kasse med mad er en betydelig hjælp i den daglige husholdning.</p>
                     <p> Derudover samarbejder vi med varmestuer og herberger i Aalborg Kommune. Andre sociale initiativer henter også overskudsmad efter behov.  </p>
