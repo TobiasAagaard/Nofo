32943ce681ebc2befd3569bdd145ebd84feda385
Tobias Aagaard <Tobias909@live.dk>
2023-12-01 11:55:07 +0100

    Button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,20 @@
+import { Link } from 'react-router-dom'
 
 import './Header.scss'
+
+import Arrow from '../../assets/images/arrow-sm-right-svgrepo-com.svg'
+
+
 export const Header = () => {
     return (
     <header className='headerContainer'>
         <div className='content'>
             <h1>NORDJYSK FØDEVAREOVERSKUD</h1>
             <h2>Vi gør en forskel for miljø og udsatte mennesker</h2>
+            <button>
+                <p>Bliv frivilig</p>
+            </button>
+           
         </div>
     </header>
     )
