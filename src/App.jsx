60710b8358d411c41937fefc7574093078ad2eff
Tobias Aagaard <Tobias909@live.dk>
2023-12-01 10:04:07 +0100

    Lidt

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Navbar } from './components/Nav/Navbar'
+import { Header } from './components/Header/Header'
+import { About } from './components/About/About'
 import { Fonde } from './components/Fonde'
 import { Partnere } from './components/Partnere'
 import { Bestyrelsen } from './components/Bestyrelsen'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
 
   return (
     <>
-    <Partnere></Partnere>
-      <Bestyrelsen></Bestyrelsen>
-      <Fonde></Fonde>
-      
+  <Navbar />
+  <Header />
+  <About />
+  <Partnere />
+  <Bestyrelsen />
+  <Fonde />
     </>
   )
-}
+} 
 
 export default App
