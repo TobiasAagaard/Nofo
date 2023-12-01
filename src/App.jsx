139363316344e7563b569d9a58518a65a07f16fc
Rasmus Krogsgaard <113165532+rasmuskrogsgaard@users.noreply.github.com>
2023-12-01 09:51:02 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+
+import { Fonde } from './components/Fonde'
+import { Partnere } from './components/Partnere'
+import { Bestyrelsen } from './components/Bestyrelsen'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  
+    <Partnere></Partnere>
+      <Bestyrelsen></Bestyrelsen>
+      <Fonde></Fonde>
+      
     </>
   )
 }
