ecded2e688e8528e10b178f1bebbcf60de634774
Rasmus Krogsgaard <113165532+rasmuskrogsgaard@users.noreply.github.com>
2023-12-05 10:00:59 +0100

    fonde

--- src/components/Fonde/Fonde.jsx
+++ src/components/Fonde/Fonde.jsx
@@ -1,59 +1,197 @@
-import style from "./fonde.module.scss"
-export const Fonde = () =>{
-    return(
-        <>
-        <div className={style.fHeader}><h2>Donationer fra fonde</h2></div>
-        <div className={style.f_gridContainer}>
-            <p className={style.fItem}>Asta og Jul. P. Justesens Fond</p>
-            <p className={style.fItem}>Chr. P. Hansen og Hustrus Fond</p>
-            <p className={style.fItem}>Civilingeniør H.C. Bechgaard og Hustru Ella Mary Bechgaards Fond</p>
-            <p className={style.fItem}>Ebba Andersen, født Kielbergs Legat</p>
-            <p className={style.fItem}>Edel og Wilhelm Daubenmerkls Almenvelgørende Fond</p>
-            <p className={style.fItem}>Ejendomsmæglernes Fond</p>
-            <p className={style.fItem}>Ellen Hørups Fond</p>
-            <p className={style.fItem}>Energiselskabet Eniig, skulderklap</p>
-            <p className={style.fItem}>Enid Ingemanns Fond</p>
-            <p className={style.fItem}>Esther Bruhns Fond</p>
-            <p className={style.fItem}>Etatsraad Georg Bestle og Hustrus Mindelegat</p>
-            <p className={style.fItem}>Fabrikant E. H. Ludvigsen og Hustrus Legat</p>
-            <p className={style.fItem}>Fonden Sol og Strand ved Margit og Kjeld</p>
-            <p className={style.fItem}>Frimodt-Heineke Fonden</p>
-            <p className={style.fItem}>Gudrun og Palle Mørchs Mindefond</p>
-            <p className={style.fItem}>Grosserer HP Strengberg og Hustru Kristiane Strengbergs Mindelegat</p>
-            <p className={style.fItem}>Grosserer, Konsul N. C. Nielsens Fond</p>
-            <p className={style.fItem}>Inge og Asker Larsens Fond</p>
-            <p className={style.fItem}>Jubilæumsfonden af 12/8 1973</p>
-            <p className={style.fItem}>Hoffmann og Husmans Fond</p>
-            <p className={style.fItem}>Lemvigh-Müller Fonden</p>
-            <p className={style.fItem}>Lund/Bugge Fonden</p>
-            <p className={style.fItem}>Marie og M. B. Richters Fond</p>
-            <p className={style.fItem}>Miljøstyrelsens pulje for Grønne Ildsjæle</p>
-            <p className={style.fItem}>Nordea Fonden</p>
-            <p className={style.fItem}>Nordlys Vækstpuljen</p>
-            <p className={style.fItem}>Melsen Fonden</p>
-            <p className={style.fItem}>Oda og Hans Svenningsens Fond</p>
-            <p className={style.fItem}>Odd-Fellow kvinder i Aalborg</p>
-            <p className={style.fItem}>Otto Bruuns Fond</p>
-            <p className={style.fItem}>Otto og Gerda Bings Mindelegat</p>
-            <p className={style.fItem}>Samsung Denmark Research Center</p>
-            <p className={style.fItem}>Socialstyrelsen</p>
-            <p className={style.fItem}>Solar Fonden af 1978</p>
-            <p className={style.fItem}>Soroptimisterne Aalborg</p>
-            <p className={style.fItem}>Spar Nord Fonden, Vejgaard afdeling</p>
-            <p className={style.fItem}>Spar Nord Fonden</p>
-            <p className={style.fItem}>Sportgoodsfonden</p>
-            <p className={style.fItem}>Sundsholmen Genbrugsbutik</p>
-            <p className={style.fItem}>Torben og Alice Frimodts Fond</p>
-            <p className={style.fItem}>Torben Vejs Fond</p>
-            <p className={style.fItem}>Y-men Aalborg 3</p>
-            <p className={style.fItem}>Aalborg Kommune</p>
-            <p className={style.fItem}>Aalborg Stiftstidendes Fond, Initiativprisen</p>
-            <p className={style.fItem}>Aase og Ejnar Danielsens Fond</p>
-            
-            
-
-        
-        </div>
-        </>
-    )
-}
+import style from "./fonde.module.scss";
+export const Fonde = () => {
+  return (
+    <>
+      <div className={style.fHeader}>
+        <h2>Donationer fra fonde</h2>
+      </div>
+      <div className={style.f_gridContainer}>
+        <p className={style.fItem}>
+          <a href="https://justesensfond.dk/" target="_blank">
+            <img src="./src/assets/fonde/astaP.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://www.aalborg.dk/" target="_blank">
+            <img
+              src="./src/assets/fonde/aalborgkommune.svg"
+              alt="aalborg kommune"
+              height="150"
+              width="150"
+            />
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="http://www.aalborgstiftstidendesfond.dk/" target="_blank">
+            <img src="./src/assets/fonde/aalborgstifstidendes.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://danielsensfond.dk/" target="_blank">
+            <img
+              src="./src/assets/fonde/danielsens_logo.svg"
+              alt="danielsens"
+              height="200"
+              width="200"
+            />
+          </a>
+        </p>
+        <p className={style.fItem}>Chr. P. Hansen og Hustrus Fond</p>
+        <p className={style.fItem}>
+          Civilingeniør H.C. Bechgaard og Hustru Ella Mary Bechgaards Fond
+        </p>
+        <p className={style.fItem}>Ebba Andersen, født Kielbergs Legat</p>
+        <p className={style.fItem}>
+          Edel og Wilhelm Daubenmerkls Almenvelgørende Fond
+        </p>
+        <p className={style.fItem}>Ejendomsmæglernes Fond</p>
+        <p className={style.fItem}>
+          <a href="https://xn--ellenhrupsfond-vqb.dk/" target="_blank">Ellen Hørups Fond</a>
+        </p>
+        <p className={style.fItem}>Energiselskabet Eniig, skulderklap</p>
+        <p className={style.fItem}>Enid Ingemanns Fond</p>
+        <p className={style.fItem}>Esther Bruhns Fond</p>
+        <p className={style.fItem}>
+          <a href="https://www.bestlesfond.dk/om-bestles-fond" target="_blank">
+            <img src="./src/assets/fonde/BestlesFond_Logo_Cobalt_RGB.webp"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://ludvigsenslegat.dk/" target="_blank">
+            Fabrikant E. H. Ludvigsen og Hustrus Legat
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://www.sologstrand.dk/information/om-os/sol-og-strand-fonden" target="_blank">
+            <img
+              src="./src/assets/fonde/sos_logo_sort-tekst-venstre.svg"
+              alt="sologstrand"
+              height="150"
+              width="150"
+            />
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://www.frimodtheinekefonden.dk/" target="_blank">
+            Frimodt-Heineke Fonden
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://www.gudrunogpallemorchsmindefond.dk/" target="_blank">
+            <img src="./src/assets/fonde/gudrunogpalle.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>
+          Grosserer HP Strengberg og Hustru Kristiane Strengbergs Mindelegat
+        </p>
+        <p className={style.fItem}>
+          <a href="http://www.nc-fond.dk/forside" target="_blank">
+            <img src="./src/assets/fonde/logo_graa.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>Inge og Asker Larsens Fond</p>
+        <p className={style.fItem}>
+          <a href="https://www.jubfond.dk/" target="_blank">Jubilæumsfonden af 12/8 1973</a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://www.hoffmannhusmansfond.dk/" target="_blank">
+            <img src="./src/assets/fonde/HoffmannHusman_Logo3.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://lmfond.dk/" target="_blank">
+            <img src="./src/assets/fonde/lmfondenbl.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>Lund/Bugge Fonden</p>
+        <p className={style.fItem}>Marie og M. B. Richters Fond</p>
+        <p className={style.fItem}>
+          <a href="https://mst.dk/erhverv" target="_blank">
+            <img
+              src="./src/assets/fonde/mmstyrelsen.png"
+              alt="mst"
+              
+            />
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://nordeafonden.dk/" target="_blank">
+            <img
+              src="./src/assets/fonde/nordeafonden.png"
+              alt="mst"
+              
+            />
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://norlys.dk/om-norlys/vaekstpulje/" target="_blank">
+            <img src="./src/assets/fonde/Norlys.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://melsenfonden.dk/" target="_blank">
+            <img src="./src/assets/fonde/Logo_top_forside.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>Oda og Hans Svenningsens Fond</p>
+        <p className={style.fItem}>
+          <a href="https://bien.oddfellow.dk/" target="_blank">
+            <img src="./src/assets/fonde/top_til_hjemmeside.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://www.ottobruunsfond.dk/" target="_blank">Otto Bruuns Fond</a>
+        </p>
+        <p className={style.fItem}>Otto og Gerda Bings Mindelegat</p>
+        <p className={style.fItem}>
+          <a href="https://www.samsung.com/dk/" target="_blank">
+            <img src="./src/assets/fonde/samsung.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://sbst.dk/" target="_blank">
+            <img
+              src="./src/assets/fonde/DK_Social- og Boligstyrelsen_logo_payoff_RGB.svg"
+              alt="Boligstyrelsen"
+              height="200"
+              width="200"
+            />
+          </a>
+        </p>
+        <p className={style.fItem}>Solar Fonden af 1978</p>
+        <p className={style.fItem}>
+          <a href="https://www.soroptimist-danmark.dk/aalborg/" target="_blank">
+            <img src="./src/assets/fonde/logo.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://www.sparnord.dk/filialer/aalborg-vejgaard" target="_blank">
+            Spar Nord Fonden, Vejgaard afdeling
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://www.sparnordfonden.dk/" target="_blank">
+            <img src="./src/assets/fonde/snf_rgb.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>
+          <a href="https://www.sportgoodsfonden.dk/" target="_blank">
+            <img src="./src/assets/fonde/sgflogo.png"></img>
+          </a>
+        </p>
+        <p className={style.fItem}>Sundsholmen Genbrugsbutik</p>
+        <p className={style.fItem}>
+          <a href="https://torbenogalicefrimodtsfond.dk/" target="_blank">
+            Torben og Alice Frimodts Fond
+          </a>
+        </p>
+        <p className={style.fItem}>Torben Vejs Fond</p>
+        <p className={style.fItem}>
+          <a href="https://aalborg3.ysmen.dk/" target="_blank">
+            <img src="./src/assets/fonde/menettes_logo_cmyk.jpg"></img>
+          </a>
+        </p>
+      </div>
+    </>
+  );
+};
